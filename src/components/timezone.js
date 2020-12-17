82f3b8597b4cf694ec136a8f265b0b2d286b4587
lucsh <me@luc.sh>
2020-12-17 17:23:54 -0300

    Delete timezone

--- src/components/timezone.js
+++ src/components/timezone.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import TimezoneDetails from './timezone-details';
 
-function Timezone({ name }) {
+function Timezone({ name, onClose }) {
   return (
     <li>
       <div className="content">
         <div className="name">{name}</div>
         <TimezoneDetails name={name} />
       </div>
+      <button type="button" tabIndex="0" onClick={() => onClose(name)}>
+        &times;
+      </button>
     </li>
   );
 }
