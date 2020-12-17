706172e8222238ce244455475bdae9d11fe15e50
lucsh <me@luc.sh>
2020-12-17 17:29:37 -0300

    remove underscore from name

--- src/components/timezone.js
+++ src/components/timezone.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import TimezoneDetails from './timezone-details';
+import parseName from '../utils/parseNames';
 
 function Timezone({ name, onClose }) {
   return (
     <li>
       <div className="content">
-        <div className="name">{name}</div>
+        <div className="name">{parseName(name)}</div>
         <TimezoneDetails name={name} />
       </div>
       <button type="button" tabIndex="0" onClick={() => onClose(name)}>
